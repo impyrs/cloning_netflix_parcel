cc68a323373d92c2b4086d2fc5b7b67fac086bfa
impyrs <impyrs@gmail.com>
2018-07-28 06:35:53 +0900

    geolocation test
    geolocation test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-document.querySelector("body").innerHTML("I love Parcel I hate webpack");
+const body = document.querySelector("body");
+
+body.innerHTML = `
+<h1>Geolocation 2000.</h1>
+<button class="js-locate">Locate me!</button>
+<div class="js-output"></div>
+`;
+
+const locateMeBtn = document.querySelector(".js-locate");
+const output = document.querySelector(".js-output");
+
+const createMapImage = (lat, lng) => {
+    const imageURL = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&size=400x400&zoom=15`;
+    const image = new Image();
+    image.src = imageURL;
+    output.appendChild(image);
+}
+
+const handleGeoError = () => {
+    output.innerHTML = "<h2>Can't locate you</h2>"
+};
+
+const handleGeoSuccess = (position) =>{
+    const {coords: {latitude, longitude}} = position;
+    output.innerHTML = "<h2>Got you...!</h2>";
+    createMapImage(latitude, longitude);
+};
+
+const getLocation = () => {
+   output.innerHTML = "<h2>Locating You...</h2>";
+   const geoOptions = {
+       enableHighAccuracy: true
+   }
+   navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError, geoOptions);
+};
+
+locateMeBtn.addEventListener("click", getLocation);
